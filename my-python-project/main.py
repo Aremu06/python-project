288ca2d606fd6a609b142ad221b2100efd560956
Tobi <85185582+Aremu06@users.noreply.github.com>
2023-04-15 13:04:00 +0200

    updating proj files

---
+++ my-python-project/main.py
@@ -0,0 +1,11 @@
+from helper import validate_and_execute
+
+user_input = ''
+while user_input != 'exit':
+    user_input = input('Hey user, enter a number of days and conversion units!\n')
+    days_and_units = user_input.split(":")
+    print(days_and_units)
+    days_and_unit_dictionary = {"days": days_and_units[0], "unit": days_and_units[1]}
+    print(days_and_unit_dictionary)
+    validate_and_execute(days_and_unit_dictionary)
+
---
+++ my-python-project/set.py
@@ -0,0 +1,15 @@
+my_set =  ["January", "February", "March"]
+#print(my_set)
+
+for element in my_set:
+    print(element)
+
+my_set.append("April")
+print(my_set)
+
+my_set.remove("January")
+print(my_set)
+
+my_list = ["January", "February", "March", "February"]
+my_list.remove("February")
+print(my_list)
